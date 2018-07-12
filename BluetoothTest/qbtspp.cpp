474f021570221f752e1b7769ac57275442559fc7
Mikołaj Werner <m.werner9205@gmail.com>
2018-07-12 21:32:31 +0200

    - added definition of method to sending strings through bluetooth

--- BluetoothTest/qbtspp.cpp
+++ BluetoothTest/qbtspp.cpp
@@ -4,3 +4,12 @@ QBTSPP::QBTSPP()
 {
 
 }
+
+void QBTSPP::openConnection(QString portName, int baud){
+
+}
+
+void QBTSPP::writeStr(QString string){
+    const QByteArray &arrayToSend = string.toLocal8Bit();
+    this->write(arrayToSend);
+}
