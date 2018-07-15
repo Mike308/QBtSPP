9c1a61d26fea9fcf216b930bc5d37f1331d8e650
Mikołaj Werner <m.werner9205@gmail.com>
2018-07-15 14:11:01 +0200

    - reimplemented declaration of worker class

--- BluetoothTest/qbtsppworker.h
+++ BluetoothTest/qbtsppworker.h
@@ -2,16 +2,31 @@
 #define QBTSPPWORKER_H
 
 #include <QObject>
+#include "qbtspp.h"
+#include <QDebug>
+
+class QBTSPP;
 
 class QBtSPPWorker : public QObject
 {
     Q_OBJECT
 public:
-    explicit QBtSPPWorker(QObject *parent = nullptr);
+
+    QBtSPPWorker(QBTSPP *bt);
+
+private:
+    QBTSPP *bt;
 
 signals:
+    void connectioStatusChanged(bool status);
+    void dataReceived(QString rxStr);
 
 public slots:
+    void onConnecting();
+    void onWriteString(QString const &str);
+    void onStringReceive();
+
+
 };
 
 #endif // QBTSPPWORKER_H
